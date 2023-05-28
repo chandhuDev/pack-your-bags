71065daa934d64cd10265c04f4a9da05c7e3338a
chandhu <chandhudev0@gmail.com>
2023-05-28 17:38:31 +0530

    adds the env

--- backend/payment.js
+++ backend/payment.js
@@ -1,38 +1,43 @@
-require('dotenv').config()
-const axios = require('axios');
+require("dotenv").config();
+const axios = require("axios");
 const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
 
-async function createPaymentIntent(req,res){
-  try{
-    const { id,seats } = req.body.items
-    const data=await axios.get(`http://localhost:1337/api/places/${id}`)
-    const cost=data.data.data.attributes.cost
-    const seatsAvailable=data.data.data.attributes.seat
+async function createPaymentIntent(req, res) {
+  try {
+    const { id, seats } = req.body.items;
+    const data = await axios.get(
+      `${process.env.STRAPI_SERVER}/api/places/${id}`
+    );
+    const cost = data.data.data.attributes.cost;
+    const seatsAvailable = data.data.data.attributes.seat;
     const paymentIntent = await stripe.checkout.sessions.create({
       line_items: [
         {
           price_data: {
-            currency: 'inr',
+            currency: "inr",
             product_data: {
-              name: 'Your Tour Cost',
+              name: "Your Tour Cost",
             },
             unit_amount: cost * 100,
           },
           quantity: seats,
         },
       ],
-      mode: 'payment',
-      success_url: `http://localhost:3000/successPage/${id}?seats=${seats}&available=${seatsAvailable}`,
-      cancel_url: 'http://localhost:3000/cancel',
+      mode: "payment",
+      success_url: `${process.FRONTEND_URL}/successPage/${id}?seats=${seats}&available=${seatsAvailable}`,
+      cancel_url: `${process.FRONTEND_URL}/cancel`,
     });
-    
+
     res.send({
-      url:paymentIntent.url
+      url: paymentIntent.url,
     });
-  }
-  catch(e){
-    console.error("Error:", e);
-  }
+  } catch (e) {
+    res.send({
+          error:{
+             message:error.message
+          }
+      })
+    }
 }
 
-module.exports={createPaymentIntent}
+module.exports = { createPaymentIntent };
